90d6e2db12348228e58494dddc9ccce083774e34
kalkidan <kalkidanteshale6@gmail.com>
2025-08-05 23:25:14 +0300

    Generate image based on user prompt

--- alx-project-0x09/pages/index.tsx
+++ alx-project-0x09/pages/index.tsx
@@ -5,13 +5,28 @@ import React, { useState } from "react";
 const Home: React.FC = () => {
 	const [prompt, setPrompt] = useState<string>("");
 	const [imageUrl, setImageUrl] = useState<string>("");
-	const [generatedImages, setGeneratedImages] = useState<ImageProps[]>([]);
 	const [isLoading, setIsLoading]= useState<boolean>(false);
 
 
 	const handleGenerateImage = async () => {
-		console.log("Generating Images")
-		console.log(process.env.NEXT_PUBLIC_GPT_API_KEY)
+		setIsLoading(true);
+		const resp = await fetch('/api/generate-image', {
+			method: 'POST',
+			body: JSON.stringify({
+				prompt
+			}),
+			headers: {
+				'Content-type': 'application/json'
+			}
+		});
+
+		if (!resp.ok) {
+			setIsLoading(false);
+			return;
+		}
+
+		const data = await resp.json();
+		setIsLoading(false);
 	};
 
 	return (
@@ -23,11 +38,11 @@ const Home: React.FC = () => {
 				</p>
 
 				<div className="w-full max-w-md">
-					<input type="text" placeholder="Enter your prompt here..." className="w-full p-3 border border-gray-300 rounded-lg mb-4" />
+					<input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Enter your prompt here..." className="w-full p-3 border border-gray-300 rounded-lg mb-4" />
 					<button onClick={handleGenerateImage} className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200">
-					{/* {
+					{
 					    isLoading ? "Loading..." : "Generate Image"
-					    } */}
+					    } 
 					   Generate Image
 					   </button>
 				</div>
